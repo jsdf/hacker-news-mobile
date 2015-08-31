ad8db6a8ac1a436176bc53a703085e28429b41e5
James Friend <james@jsdf.co>
2015-08-31 20:44:26 +1000

    added logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var React = require('react')
 var Router = require('react-router')
+var morgan = require('morgan')
 
 var routes = require('./components/routes')
 var getRoutesInitialData = require('./util/get-routes-initial-data')
@@ -15,13 +16,12 @@ var engines = require('consolidate')
 app.engine('html', engines.hogan)
 app.set('view engine', 'html')
 app.set('views', __dirname + '/server/views')
+app.use(morgan('combined'))
 app.use(express.static('assets', {maxAge: '1 month'}))
 
-// hacky way of preventing bad asset requests from hitting react router
-app.get(/.*\.\w+$/, (req, res) => res.sendStatus(404))
-
-app.use((req, res) => {
+const renderRoute = (req, res) => {
   Router.run(routes, req.url, (Handler, routerState) => {
+
     getRoutesInitialData(routerState)
       .then((routesInitialData) => {
         res.set('Cache-Control', 'public, max-age=10000')
@@ -33,7 +33,10 @@ app.use((req, res) => {
         })
       })
   })
-})
+}
+
+app.get('/', renderRoute)
+app.get('/story/:id', renderRoute)
 
 const LISTEN_PORT = process.env.PORT || config.port || 3000
-app.listen(LISTEN_PORT, () => console.log('it lives'))
+app.listen(LISTEN_PORT)
