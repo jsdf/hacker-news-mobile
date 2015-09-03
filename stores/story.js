eba9de985b57397d45706597637459407fbf5fc2
James Friend <james@jsdf.co>
2015-09-03 23:25:03 +1000

    added inlined config

--- stores/story.js
+++ stores/story.js
@@ -1,7 +1,7 @@
 var urlJoin = require('url-join')
 var CollectionStore = require('./collection')
 var fetch = require('../util/fetch')
-var config = require('../config.json')
+var config = require('../util/config')
 
 const API_PATH = urlJoin(config.apiHost, '/item')
 
--- stores/top-story.js
+++ stores/top-story.js
@@ -2,7 +2,7 @@ var _ = require('underscore')
 var urlJoin = require('url-join')
 var CollectionStore = require('./collection')
 var fetch = require('../util/fetch')
-var config = require('../config.json')
+var config = require('../util/config')
 var logErr = require('../util/logErr')
 
 const API_PATH = urlJoin(config.apiHost, '/')
